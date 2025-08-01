aeca0a389306e80c554eb13b7fbfd96f89bf39f7
chahyeonmin91 <chaheonmin91@gmail.com>
2025-08-01 19:15:06 +0900

    m

--- src/main/java/com/wassupshoppingmall/global/config/WebConfig.java
+++ src/main/java/com/wassupshoppingmall/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080", "https://프론트주소", "https://wassup-shoppingmall.cloudtype.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
