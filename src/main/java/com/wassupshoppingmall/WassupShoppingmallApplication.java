061bf8c478795d5311f3af45e0736f5ac58872c4
chahyeonmin91 <chaheonmin91@gmail.com>
2025-08-10 21:29:45 +0900

    yml

--- src/main/java/com/wassupshoppingmall/WassupShoppingmallApplication.java
+++ src/main/java/com/wassupshoppingmall/WassupShoppingmallApplication.java
@@ -3,7 +3,7 @@ package com.wassupshoppingmall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.wassupshoppingmall")
+@SpringBootApplication
 public class WassupShoppingmallApplication {
 
     public static void main(String[] args) {
