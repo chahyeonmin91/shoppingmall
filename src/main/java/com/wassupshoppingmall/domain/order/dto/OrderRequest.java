e11048bb8e4ac8bfb1ee688d6ecb34b46e32d092
chahyeon min <chahyeonmin91@gmail.com>
2025-08-17 22:09:16 +0900

    order

--- src/main/java/com/wassupshoppingmall/domain/order/dto/OrderRequest.java
+++ src/main/java/com/wassupshoppingmall/domain/order/dto/OrderRequest.java
@@ -1,5 +1,7 @@
 package com.wassupshoppingmall.domain.order.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.util.List;
@@ -7,14 +9,19 @@ import java.util.List;
 @Getter
 public class OrderRequest {
 
+    @NotBlank
     private String recipient;
+    @NotBlank
     private String phone;
+    @NotBlank
     private String address;
+
     private List<OrderProduct> products;
 
     @Getter
     public static class OrderProduct{
         private Long productId;
+        @Min(1)
         private int quantity;
     }
 }
